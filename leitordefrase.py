df6323af1f078de669c259cc73e61108ba5ba5d4
Savio-888 <saviopereirasoares6@gmail.com>
2026-02-26 18:33:17 -0300

    Adicionando leitordefrase.py
    Um leitor de frase que conta quantas palavras a frase inserida tem.

---
+++ leitordefrase.py
@@ -0,0 +1,2 @@
+# Faça um programa que pegue uma frase inserida pelo usuário e conte o total de palavras dessa frase
+print(f'Muito Bom! A frase digitada contém um total de {len(input('Digite uma mensagem qualquer que você queira compartilhar.\n>> ').split())} palavras.')
